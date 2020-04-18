6b733d50c3c1d73a02154f9a3fe0d55d9b3baf19
pz <porya.aut1984@gmail.com>
2020-04-19 00:20:37 +0430

    increase like class

--- src/App.js
+++ src/App.js
@@ -4,11 +4,25 @@ import './App.css';
 
 
 class Status extends React.Component {
+  constructor() {
+    super();
+    this.state ={
+    likes : 0
+  }
+  this.like = this.like.bind(this);
+  }
+  like()
+   {
+     this.setState({
+          likes : this.state.likes+1
+})
+   }
   render(){
     return(
         <div className="Status">
-            <p> this is my statuse ......................</p>
-            <p> <button> like </button> </p>
+            <p> {this.props.t} </p>
+            <p> <button onClick={this.like}>
+             {this.state.likes} like </button> </p>
 
         </div>
 
@@ -23,8 +37,8 @@ class App extends React.Component {
     <div className="App">
       <header className="App-header">
 
-          <Status />
-          <Status />
+          <Status  t="wwwwwwwwwwwwwwwwww" />
+          <Status t="ddddddddddeeeeeeeee" />
 
       </header>
     </div>
